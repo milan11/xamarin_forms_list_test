66cab300262c87a5e8584e39d07e282140a11199
Milan Laslop <milan33@gmail.com>
2017-03-28 15:11:08 +0200

    initial

---
+++ App4/App4/ListPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+
+            var items = new List<object>();
+
+            for (int i = 0; i < 20; ++i)
+            {
+                items.Add(new object());
+            }
+
+            SomeList.ItemsSource = items;
+        }
+    }
+}
